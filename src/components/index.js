61d62c5c9b31ed4cddeab4466aeca78d6c8f20b7
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2019-11-14 14:45:33 +0100

    CY-1489 - Added default exports for all components (#7)
    * Added default exports for all components
    * Updated Pagination style

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,48 @@
+/* Buttons */
 export { default as ApproveButton } from './buttons/ApproveButton';
 export { default as CancelButton } from './buttons/CancelButton';
 export { default as CopyToClipboardButton } from './buttons/CopyToClipboardButton';
 
-export { default as Checkmark } from './elements/Checkmark';
-export { default as Loading } from './elements/Loading';
+/* Cloudify */
+export { default as ResourceVisibility } from './cloudify/visibility/ResourceVisibility';
+export { default as VisibilityField } from './cloudify/visibility/VisibilityField';
+export { default as VisibilityIcon } from './cloudify/visibility/VisibilityIcon';
 
+/* Data */
+export { default as DataSegment } from './data/DataSegment';
+export { default as DataTable } from './data/DataTable';
+export { default as HighlightText } from './data/HighlightText';
+export { default as KeyIndicator } from './data/KeyIndicator';
+export { default as NodesTree } from './data/NodesTree';
+
+/* Elements */
+export { default as Checkmark } from './elements/Checkmark';
+export { default as Dropdown } from './elements/Dropdown';
+export { default as EditableLabel } from './elements/EditableLabel';
+export { default as ErrorMessage } from './elements/ErrorMessage';
+export { default as Loading } from './elements/Loading';
+export { default as Menu } from './elements/Menu';
+
+/* Form */
+export { default as Checkbox } from './form/Checkbox';
+export { default as ColorPicker } from './form/ColorPicker';
+export { default as DateInput } from './form/DateInput';
+export { default as DatePicker } from './form/DatePicker';
+export { default as DateRangeInput } from './form/DateRangeInput';
+export { default as FieldLabel } from './form/FieldLabel';
+export { default as FileInput } from './form/FileInput';
+export { default as Form } from './form/Form';
+export { default as JsonInput } from './form/JsonInput';
+export { default as TimeInput } from './form/TimeInput';
+export { default as UrlOrFileInput } from './form/UrlOrFileInput';
+
+/* Modals */
 export { default as Alert } from './modal/Alert';
 export { default as Confirm } from './modal/Confirm';
 export { default as ReadmeModal } from './modal/ReadmeModal';
+
+/* Popups */
+export { default as Popup } from './popups/Popup';
+export { default as PopupConfirm } from './popups/PopupConfirm';
+export { default as PopupHelp } from './popups/PopupHelp';
+export { default as PopupMenu } from './popups/PopupMenu';

8652b31126d9a6cf8b710c095cc6d48e933ed4f1
Jakub Madej <jakub.madej@amartus.com>
2019-11-26 09:14:31 +0100

    Updated UrlOrFileInput for easier reuse (will require minor changes in stage)

--- src/components/form/UrlOrFileInput/UrlOrFileInput.stories.jsx
+++ src/components/form/UrlOrFileInput/UrlOrFileInput.stories.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Label } from 'semantic-ui-react';
 import Form from '../Form/Form';
 
 export default {
@@ -9,17 +8,13 @@ export default {
 
 export const basic = () => (
     <Form>
-        <Form.Field label="File">
+        <Form.Field label="File or URL">
             <Form.UrlOrFile
                 name="blueprint"
-                value=""
                 placeholder="Provide the blueprint's URL or click browse to select a file"
                 onChangeUrl={() => {}}
-                onFocusUrl={() => {}}
                 onBlurUrl={() => {}}
                 onChangeFile={() => {}}
-                onResetFile={() => {}}
-                label={<Label>URL or File</Label>}
             />
         </Form.Field>
     </Form>
